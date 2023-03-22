683729ea431c7c67e66caba85515530fbb60cab1
GoofyGuy1 <125322588+GoofyGuy1@users.noreply.github.com>
2023-03-22 18:18:24 -0500

    Update romlist.js

--- dist/romlist.js
+++ dist/romlist.js
@@ -1,7 +1,4 @@
 var ROMLIST = [
-    /*
-    {url:"roms/rom1.z64",title:"Game 1"},
-    {url:"roms/rom2.v64",title:"Game 2"},
-    {url:"roms/rom3.v64",title:"Game 3"},
-    */
+    {url:"roms/mariokart.z64",title:"Mario Kart 64"},
+    {url:"roms/smashbros.z64",title:"Super Smash Bros 64"},
 ];
